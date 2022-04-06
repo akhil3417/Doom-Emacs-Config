3be40008ec3ef270488df17cea9f24555f125faf
akhil3417 <akhilpratapsingh3417@gmail.com>
2022-04-06 10:55:43 +0530

    add system packages managment clean up code

--- exwm/start-exwm.sh
+++ exwm/start-exwm.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# [[file:../config.org::*launcher script v2][launcher script v2:1]]
 #!/bin/bash
 # Very important: Avoid spawning daemons here.
 # They will not exit with this process, so we will no longer have a clean X11 shutdown.
@@ -86,3 +87,4 @@ export _JAVA_AWT_WM_NONREPARENTING=1
 # exec emacs --daemon && emacsclient -c --eval "(exwm-enable)"
 exec dbus-launch --exit-with-session emacs --eval "(exwm-enable)"
 # exec emacsclient -c
+# launcher script v2:1 ends here
