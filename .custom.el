5a54a8198b8ec6d96105103243d6d41b692d5d9f
akhil3417 <akhilpratapsingh3417@gmail.com>
2022-04-27 04:20:31 +0530

    idk

--- .custom.el
+++ .custom.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(elfeed-feeds '("https://www.archlinux.org/feeds/news/"))
  '(hacker-typer-remove-comments t)
  '(hacker-typer-type-rate 1))
 (custom-set-faces
