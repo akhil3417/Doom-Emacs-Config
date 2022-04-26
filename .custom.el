d03c7fff8a017f7f095ee56fba0e57d1cd93458a
akhil3417 <akhilpratapsingh3417@gmail.com>
2022-04-27 03:10:37 +0530

    add custom.el

---
+++ .custom.el
@@ -0,0 +1,37 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elfeed-feeds '("https://www.archlinux.org/feeds/news/"))
+ '(hacker-typer-remove-comments t)
+ '(hacker-typer-type-rate 1))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doom-modeline-buffer-modified ((t (:foreground "orange"))))
+ '(font-lock-comment-face ((t (:slant italic))))
+ '(font-lock-keyword-face ((t (:slant italic))))
+ '(mode-line ((t (:height 0.9))))
+ '(mode-line-inactive ((t (:height 0.9))))
+ '(org-block (nil :foreground nil :inherit 'fixed-pitch))
+ '(org-checkbox (nil :inherit 'fixed-pitch))
+ '(org-code (nil :inherit '(shadow fixed-pitch)))
+ '(org-document-title ((t (:height 1.2))))
+ '(org-level-1 ((t (:inherit outline-1 :weight extra-bold :height 1.35))))
+ '(org-level-2 ((t (:inherit outline-2 :weight bold :height 1.25))))
+ '(org-level-3 ((t (:inherit outline-3 :weight bold :height 1.22))))
+ '(org-level-4 ((t (:inherit outline-4 :weight bold :height 1.19))))
+ '(org-level-5 ((t (:inherit outline-5 :weight semi-bold :height 1.16))))
+ '(org-level-6 ((t (:inherit outline-6 :weight semi-bold :height 1.13))))
+ '(org-level-7 ((t (:inherit outline-7 :weight semi-bold))))
+ '(org-level-8 ((t (:inherit outline-8 :weight semi-bold))))
+ '(org-meta-line (nil :inherit '(font-lock-comment-face fixed-pitch)))
+ '(org-special-keyword (nil :inherit '(font-lock-comment-face fixed-pitch)))
+ '(org-table (nil :inherit '(shadow fixed-pitch)))
+ '(org-verbatim (nil :inherit '(shadow fixed-pitch))))
+(put 'set-goal-column 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
+(put 'customize-group 'disabled nil)
