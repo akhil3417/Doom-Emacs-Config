4adb43526163d14dc718bb79b7924982e0e7220f
akhil3417 <akhilpratapsing3417@gmail.com>
2023-02-14 10:11:12 +0530

    add vertico postframe config

--- lisp/setup-vertico.el
+++ lisp/setup-vertico.el
@@ -155,6 +155,26 @@
   :config
   (setq vertico-buffer-display-action 'display-buffer-reuse-window))
 
+;; vertico-posframe config
+;; (require 'vertico-posframe)
+;; (vertico-posframe-mode 1)
+;; (setq vertico-multiform-commands
+;;       '((consult-line
+;;          posframe
+;;          (vertico-posframe-poshandler . posframe-poshandler-frame-top-center)
+;;          (vertico-posframe-border-width . 10)
+;;          ;; NOTE: This is useful when emacs is used in both in X and
+;;          ;; terminal, for posframe do not work well in terminal, so
+;;          ;; vertico-buffer-mode will be used as fallback at the
+;;          ;; moment.
+;;          (vertico-posframe-fallback-mode . vertico-buffer-mode))
+;;         (t posframe)))
+;; (vertico-multiform-mode 1)
+
+;; (setq vertico-posframe-parameters
+;;       '((left-fringe . 8)
+;;         (right-fringe . 8)))
+
 (provide 'setup-vertico)
 ;; setup-vertico.el ends here
 ;; vertico:1 ends here
