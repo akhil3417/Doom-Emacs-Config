a09a9c42b810af6bf151588fea001bd2ee424682
akhil3417 <akhilpratapsingh3417@gmail.com>
2023-08-24 21:35:01 +0530

    configure electric pair to improve usablity

--- config.el
+++ config.el
@@ -1448,6 +1448,26 @@ preview-default-preamble "\\fi}\"%' \"\\detokenize{\" %t \"}\""))
       (+latex-fold-last-macro-a)))
   (add-hook 'aas-post-snippet-expand-hook #'laas-tex-fold-maybe))
 ;; LAAS:2 ends here
+  (setq electric-pair-inhibit-predicate 'electric-pair-conservative-inhibit)
+  (setq electric-pair-preserve-balance t)
+  (setq electric-pair-pairs
+        '((8216 . 8217)
+          (8220 . 8221)
+          (171 . 187)))
+  (setq electric-pair-skip-self 'electric-pair-default-skip-self)
+  (setq electric-pair-skip-whitespace nil)
+  (setq electric-pair-skip-whitespace-chars '(9 10 32))
+  (setq electric-quote-context-sensitive t)
+  (setq electric-quote-paragraph t)
+  (setq electric-quote-string nil)
+  (setq electric-quote-replace-double t)
+  (electric-pair-mode -1)
+  (electric-quote-mode -1)
+  ;; I don't like auto indents in Org and related.  They are okay for
+  ;; programming.
+  (electric-indent-mode -1)
+  (add-hook 'prog-mode-hook #'electric-indent-local-mode))
+
 (use-package! paren
   :ensure nil
   :config
