ceada143016c53c73d671c9800a9a0982d41b5f0
akhil3417 <akhilpratapsingh3417@gmail.com>
2023-09-25 21:52:17 +0530

    remove cz already implemented in my fork

--- lisp/prot-eww.el
+++ lisp/prot-eww.el
@@ -60,17 +60,6 @@
 ;; TODO 2021-10-15: Deprecate this in favour of what we added to Emacs29.
 ;; <https://protesilaos.com/codelog/2021-10-15-emacs-29-eww-rename-buffers/>.
 
-(defun prot-eww--rename-buffer ()
-  "Rename EWW buffer using page title or URL.
-To be used by `eww-after-render-hook'."
-  (let ((name (if (eq "" (plist-get eww-data :title))
-                  (plist-get eww-data :url)
-                (plist-get eww-data :title))))
-    (rename-buffer (format "*%s # eww*" name) t)))
-
-(add-hook 'eww-after-render-hook #'prot-eww--rename-buffer)
-(advice-add 'eww-back-url :after #'prot-eww--rename-buffer)
-(advice-add 'eww-forward-url :after #'prot-eww--rename-buffer)
 
 ;;;; History extras
 
@@ -332,106 +321,6 @@ When called from an eww buffer, provide the current link as
 ;; bookmark.el.  Further below I provide integration with the latter,
 ;; meaning that we must either make this obsolete or make it work with
 ;; the new system.
-;;;###autoload
-(defun prot-eww-visit-bookmark (&optional arg)
-  "Visit bookmarked URL.
-
-With optional prefix ARG (\\[universal-argument]) open URL in a
-new EWW buffer."
-  (interactive "P")
-  (eww-read-bookmarks)
-  (let ((list (gensym)))
-    (dolist (bookmark eww-bookmarks)
-      (push (plist-get bookmark :url) list))
-    (if eww-bookmarks
-        ;; (eww (completing-read "Visit EWW bookmark: " list)
-        (eww (ivy-completing-read "Visit EWW bookmark: " list) ;use ivy completion instead for this function
-             (when arg 4))
-      (user-error "No bookmarks"))))
-
-(defun prot-eww--capture-url-on-page (&optional position)
-  "Capture all the links on the current web page.
-
-Return a list of strings.  Strings are in the form LABEL @ URL.
-When optional argument POSITION is non-nil, include position info
-in the strings too, so strings take the form
-LABEL @ URL ~ POSITION."
-  (let (links match)
-    (save-excursion
-      (goto-char (point-max))
-      ;; NOTE 2021-07-25: The first clause in the `or' is meant to
-      ;; address a bug where if a URL is in `point-min' it does not get
-      ;; captured.
-      (while (setq match (text-property-search-backward 'shr-url))
-        (let* ((raw-url (prop-match-value match))
-               (start-point-prop (prop-match-beginning match))
-               (end-point-prop (prop-match-end match))
-               (url (when (stringp raw-url)
-                      (propertize raw-url 'face 'link)))
-               (label (replace-regexp-in-string "\n" " " ; NOTE 2021-07-25: newlines break completion
-                                                (buffer-substring-no-properties
-                                                 start-point-prop end-point-prop)))
-               (point start-point-prop)
-               (line (line-number-at-pos point t))
-               (column (save-excursion (goto-char point) (current-column)))
-               (coordinates (propertize
-                             (format "%d,%d (%d)" line column point)
-                             'face 'shadow)))
-          (when url
-            (if position
-                (push (format "%-15s ~ %s  @ %s"
-                              coordinates label url)
-                      links)
-              (push (format "%s  @ %s"
-                            label url)
-                    links))))))
-    links))
-
-(defmacro prot-eww-act-visible-window (&rest body)
-  "Run BODY within narrowed-region.
-If region is active run BODY within active region instead.
-Return the value of the last form of BODY."
-  `(save-restriction
-     (if (use-region-p)
-         (narrow-to-region (region-beginning) (region-end))
-       (narrow-to-region (window-start) (window-end)))
-     ,@body))
-
-;;;###autoload
-(defun prot-eww-visit-url-on-page (&optional arg)
-  "Visit URL from list of links on the page using completion.
-
-With optional prefix ARG (\\[universal-argument]) open URL in a
-new EWW buffer."
-  (interactive "P")
-  (when (derived-mode-p 'eww-mode)
-    (let* ((links (prot-eww--capture-url-on-page))
-           (selection (completing-read "Browse URL from page: " links nil t))
-           (url (replace-regexp-in-string ".*@ " "" selection)))
-      (eww url (when arg 4)))))
-
-;;;###autoload
-(defun prot-eww-jump-to-url-on-page (&optional arg)
-  "Jump to URL position on the page using completion.
-
-When called without ARG (\\[universal-argument]) get URLs only
-from the visible portion of the buffer.  But when ARG is provided
-consider whole buffer."
-  (interactive "P")
-  (when (derived-mode-p 'eww-mode)
-    (let* ((links
-            (if arg
-                (prot-eww--capture-url-on-page t)
-              (prot-eww-act-visible-window
-               (prot-eww--capture-url-on-page t))))
-           (prompt-scope (if arg
-                             (propertize "URL on the page" 'face 'warning)
-                           "visible URL"))
-           (prompt (format "Jump to %s: " prompt-scope))
-           (selection (completing-read prompt links nil t))
-           (position (replace-regexp-in-string "^.*(\\([0-9]+\\))[\s\t]+~" "\\1" selection))
-           (point (string-to-number position)))
-      (goto-char point))))
 
 (defvar prot-eww--occur-feed-regexp
   (concat "\\(rss\\|atom\\)\\+xml.\\(.\\|\n\\)"
@@ -722,29 +611,6 @@ result in a new buffer."
     (eww eww-pass arg)))
 
 ;;;###autoload
-(defun prot-eww-open-in-other-window ()
-  "Use `eww-open-in-new-buffer' in another window."
-  (interactive)
-  (other-window-prefix)       ; For emacs28 -- it's a hack, but why not?
-  (eww-open-in-new-buffer))
-
-(defun default-browser-eww()
-  "Set eww to be default browser."
-  (interactive)
-  (setq engine/browser-function 'eww-browse-url;;  browse-url-default-browser open firefox
-       browse-url-browser-function 'eww-browse-url
-        browse-url-secondary-browser-function 'browse-url-generic))
-
-(defun default-browser-qutebrowser()
-  (interactive)
-  (setq engine/browser-function 'browse-url-generic;;  browse-url-default-browser open firefox
-        browse-url-browser-function 'browse-url-generic))
-
-(defun default-browser-firefox()
-  (interactive)
-  (setq engine/browser-function 'browse-url-default-browser;;  browse-url-default-browser open firefox
-        browse-url-browser-function 'browse-url-default-browser))
-;;;###autoload
 (defun eww-search-in-other-window ()
   "Use `eww-open-in-new-buffer' in another window."
   (interactive)
@@ -773,52 +639,6 @@ images."
 ;; bookmark.el.  Further below I provide integration with the latter,
 ;; meaning that we must either make this obsolete or make it work with
 ;; the new system.
-;;;###autoload
-(defun prot-eww-bookmark-page (title)
-  "Add eww bookmark named with TITLE."
-  (interactive
-   (list
-    (read-string "Set bookmark title: " (plist-get eww-data :title))))
-  (plist-put eww-data :title title)
-  (eww-add-bookmark))
-
-(defvar prot-eww--punctuation-regexp "[][{}!@#$%^&*()_=+'\"?,.\|;:~`‘’“”]*"
-  "Regular expression of punctionation that should be removed.")
-
-(defun prot-eww--slug-no-punct (str)
-  "Convert STR to a file name slug."
-  (replace-regexp-in-string prot-eww--punctuation-regexp "" str))
-
-(defun prot-eww--slug-hyphenate (str)
-  "Replace spaces with hyphens in STR.
-Also replace multiple hyphens with a single one and remove any
-trailing hyphen."
-  (replace-regexp-in-string
-   "-$" ""
-   (replace-regexp-in-string
-    "-\\{2,\\}" "-"
-    (replace-regexp-in-string "--+\\|\s+" "-" str))))
-
-(defun prot-eww--sluggify (str)
-  "Make STR an appropriate file name slug."
-  (downcase (prot-eww--slug-hyphenate (prot-eww--slug-no-punct str))))
-
-;;;###autoload
-(defun prot-eww-download-html (name)
-  "Download web page and call the file with NAME."
-  (interactive
-   (list
-    (prot-eww--sluggify
-     (read-string "Set downloaded file name: " (plist-get eww-data :title)))))
-  (let* ((path (thread-last eww-download-directory
-                 (expand-file-name
-                  (concat (format-time-string "%Y%m%d_%H%M%S") "--" name ".html"))))
-         (out (prot-common-shell-command-with-exit-code-and-output
-               "wget" "-q" (format "%s" (plist-get eww-data :url))
-               "-O" (format "%s" (shell-quote-argument path)))))
-    (if (= (car out) 0)
-        (message "Downloaded page at %s" path)
-      (message "Error downloading page: %s" (cdr out)))))
 
 (defun prot-eww--kill-buffers-when (predicate)
   "Kill buffers when PREDICATE is non-nil.
--- lisp/setup-eww.el
+++ lisp/setup-eww.el
@@ -38,33 +38,10 @@
 ;;   (start-process "mpv" nil "mpv" (shell-quote-argument url)))
 
 
-(use-package! goto-addr
-  :config
-  (setq goto-address-url-face 'link)
-  (setq goto-address-url-mouse-face 'highlight)
-  (setq goto-address-mail-face nil)
-  (setq goto-address-mail-mouse-face 'highlight))
-(use-package! shr
-  :config
-  (setq shr-use-colors nil)             ; t is bad for accessibility
-  (setq shr-use-fonts nil)              ; t is not for me
-  (setq shr-max-image-proportion 0.6)
-  (setq shr-image-animate nil)          ; No GIFs, thank you!
-  (setq shr-indentation 2)                           ; Left-side margin
-  (setq shr-width nil)                  ; check `prot-eww-readable'
-  (setq shr-discard-aria-hidden t)
-  (setq shr-cookie-policy nil))
 
-(use-package! url-cookie
-  :config
-  (setq url-cookie-untrusted-urls '(".*")))
 
 (use-package! eww
   :config
-  (setq eww-restore-desktop t)
-  (setq eww-desktop-remove-duplicates t)
-  (setq eww-header-line-format nil)
-  (setq eww-search-prefix "https://duckduckgo.com/html/?q=")
   (setq eww-download-directory (expand-file-name "~/Documents/eww-downloads"))
   (setq eww-suggest-uris
         '(eww-links-at-point
@@ -72,13 +49,10 @@
   ;; (setq eww-bookmarks-directory (locate-user-emacs-file "eww-bookmarks/"))
   (setq eww-bookmarks-directory (expand-file-name "~/.config/eww-bookmarks/"))
   (setq bookmark-file (expand-file-name "~/.config/eww-bookmarks/emacs-bookmarks"))
-  (setq eww-history-limit 150)
   (setq eww-use-external-browser-for-content-type
         "\\`\\(video/\\|audio\\)") ; On GNU/Linux check your mimeapps.list
    ;; "\\`\\(video/\\|audio/\\|application/ogg\\|application/pdf\\)"))
-  (setq eww-browse-url-new-window-is-tab nil)
-  (setq eww-form-checkbox-selected-symbol "[X]")
-  (setq eww-form-checkbox-symbol "[ ]")
+   ;;
   ;; NOTE `eww-retrieve-command' is for Emacs28.  I tried the following
   ;; two values.  The first would not render properly some plain text
   ;; pages, such as by messing up the spacing between paragraphs.  The
@@ -89,15 +63,14 @@
   ;;
   ;; '("wget" "--quiet" "--output-document=-")
   ;; '("chromium" "--headless" "--dump-dom")
-  ;; (setq eww-retrieve-command nil)
 
 
 
 ;; list buffers gt
 
   (define-key eww-link-keymap (kbd "v") nil) ; stop overriding `eww-view-source'
-  (define-key eww-mode-map (kbd "L") #'eww-list-bookmarks) ;g b
-  (define-key dired-mode-map (kbd "E") #'eww-open-file) ; to render local HTML files
+  (after! dired
+  (define-key dired-mode-map (kbd "E") #'eww-open-file)) ; to render local HTML files
   (define-key eww-buffers-mode-map (kbd "d") #'eww-buffer-kill)   ; it actually deletes
   (define-key eww-bookmark-mode-map (kbd "d") #'eww-bookmark-kill)) ; same
 
@@ -124,28 +97,11 @@
 ;;keybinding are almost same to qutebrowser for consistency
 (map! :leader
       :desc "Eww" "@ " #'prot-eww-map)
-(evil-define-key 'normal eww-mode-map  (kbd "m") #'prot-eww-bookmark-page
-                                               (kbd "D") #'prot-eww-download-html
-                                               (kbd "C-F") #'prot-eww-find-feed
+(evil-define-key 'normal eww-mode-map        (kbd "C-F") #'prot-eww-find-feed
                                               (kbd ".") #'prot-eww-list-history ;key fine
 
-                                            (kbd "H") #'eww-back-url ;key fine
-                                              (kbd "L") #'eww-forward-url ;key fine
-                                               (kbd "O") #'prot-eww-visit-bookmark
-                                               (kbd "o") #'eww-search-words
-                                               (kbd "r") #'eww-reload
-                                               (kbd "g t") #'eww-switch-to-buffer
-                                               (kbd "g T") #'eww-list-buffer
                                                (kbd "t") #'prot-eww-search-engine
-                                               (kbd "C-E") #'eww-browse-with-external-browser
-                                               (kbd "M") #'browse-url-at-point-umpv
-                                               (kbd "C-e") #'prot-eww-browse-dwim
-                                               (kbd "C-f") #'prot-eww-visit-url-on-page
-                                               (kbd "f") #'prot-eww-jump-to-url-on-page;;visible
                                                (kbd "C-r") #'prot-eww-readable
-                                               (kbd "C-Y") #'link-hint-copy-link-at-point
-                                               (kbd "y l") #'link-hint-copy-link-at-point
-                                               (kbd "y p") #'eww-copy-page-url
                                                (kbd "Q") #'prot-eww-quit)
 
 
@@ -153,8 +109,5 @@
                                                (kbd "o") #'prot-eww-open-in-other-window)
 
 
-(defun my-open-link-at-point-in-external-browser ()
-  (interactive)
-  (link-hint-copy-link-at-point)
-  (let ((url (current-kill 0)))
-    (browse-url-generic url)))
+
+
