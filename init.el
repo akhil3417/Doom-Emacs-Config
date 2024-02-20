0f906d2f71bdfbfc4812a08c3a39502468a91118
akhil3417 <akhilpratapsingh3417@gmail.com>
2024-02-20 10:34:52 +0530

    update init.el

--- init.el
+++ init.el
@@ -65,7 +65,7 @@
        ;;lispy             ; vim for lisp, for people who don't like vim
        multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
-       ;;parinfer          ; turn lisp into python, sort of
+       ;; parinfer          ; turn lisp into python, sort of
        rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
        word-wrap        ; soft wrapping with language-aware indent
@@ -76,7 +76,7 @@
        ibuffer         ; interactive buffer management
        undo                ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
-       (eww +shrface +highlight +langdetect)               ;better than chrome
+       (eww +highlight +langdetect)               ;better than chrome
 
        :term
        eshell            ; the elisp shell that works everywhere
@@ -93,53 +93,53 @@
 
        :tools
        (codeium)
-      (gptel +aipihkal +extras +extension)
+      (gptel +aipihkal +anyscale +togetherai +gemini +extras +extension)
        ;;ansible
        ;;biblio            ; Writes a PhD for you (citation needed)
        ;;collab            ; buffers with friends
-       (debugger +lsp)          ; FIXME stepping through code, to help you add bugs
+      (debugger +lsp)          ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       docker
+      docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
-       (eval +overlay)     ; run code, run (also, repls)
+      (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
-       (lookup                      ; helps you navigate your code and documentation
-        +dictionary                 ; dictionary/thesaurus is nice
-        +offline
-        +docsets)                   ; ...or in Dash docsets locally
-       (lsp +elgot +peek)               ; M-x vscode
-       (magit +forge)             ; a git porcelain for Emacs
-       make              ; run make tasks from Emacs
-       (pass +auth)              ; password manager for nerds
-       pdf               ; pdf enhancements
+      (lookup                      ; helps you navigate your code and documentation
+       +dictionary                 ; dictionary/thesaurus is nice
+       +offline
+       +docsets)                   ; ...or in Dash docsets locally
+      (lsp +elgot +peek)               ; M-x vscode
+      (magit +forge)             ; a git porcelain for Emacs
+      make              ; run make tasks from Emacs
+      (pass +auth)              ; password manager for nerds
+      pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
-       rgb               ; creating color strings
+      rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
-       tree-sitter       ; syntax and parsing, sitting in a tree...
+      tree-sitter       ; syntax and parsing, sitting in a tree...
        ;;upload            ; map local to remote projects via ssh/ftp
 
-       :os
-       (:if IS-MAC macos)  ; improve compatibility with macOS
-       tty               ; improve the terminal Emacs experience
+      :os
+      (:if IS-MAC macos)  ; improve compatibility with macOS
+      tty               ; improve the terminal Emacs experience
 
-       :lang
+      :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
-       (cc +lsp)         ; C > C++ == 1
+      (cc +lsp)         ; C > C++ == 1
        ;;clojure           ; java with a lisp
-       common-lisp       ; if you've seen one lisp, you've seen them all
+      common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
-       data              ; config/data formats
+      data              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;dhall
        ;;elixir            ; erlang done right
        ;;elm               ; care for a cup of TEA?
-       emacs-lisp        ; drown in parentheses
+      emacs-lisp        ; drown in parentheses
        ;;erlang            ; an elegant language for a more civilized age
        ;;ess               ; emacs speaks statistics
        ;;factor
@@ -150,74 +150,75 @@
        ;;gdscript          ; the language you waited for
        ;;(go +lsp)         ; the hipster dialect
        ;;(graphql +lsp)    ; Give queries a REST
-       (haskell +lsp)    ; a language that's lazier than I am
+      (haskell +lsp)    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
-       json              ; At least it ain't XML
+      json              ; At least it ain't XML
        ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
-       javascript        ; all(hope(abandon(ye(who(enter(here))))))
+      javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       latex             ; writing papers in Emacs has never been so fun
+      latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
-       lua               ; one-based indices? one-based indices
-       markdown          ; writing docs for people to ignore
+      lua               ; one-based indices? one-based indices
+      markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        ;; (org +pretty +pomodoro +dragndrop +present +hugo +pandoc +journal +roam2)               ; organize your plain life in plain text
-       (org                         ; organize your plain life in plain text
-        +dragndrop                  ; drag & drop files/images into org buffers
-        +notify
-        ;; +hugo                     ; use Emacs for hugo blogging
-        +journal
-        +noter                      ; enhanced PDF notetaking
-        +jupyter                    ; ipython/jupyter support for babel
-        +pandoc                     ; export-with-pandoc support
-        ;; +gnuplot                    ; who doesn't like pretty pictures
-        +pomodoro                 ; be fruitful with the tomato technique
-        +present                    ; using org-mode for presentations
-        ;; +pretty
-        +roam2)                     ; wander around notes
+      (org                         ; organize your plain life in plain text
+       +dragndrop                  ; drag & drop files/images into org buffers
+       +notify
+       ;; +hugo                     ; use Emacs for hugo blogging
+       +journal
+       +noter                      ; enhanced PDF notetaking
+       +jupyter                    ; ipython/jupyter support for babel
+       +pandoc                     ; export-with-pandoc support
+       ;; +gnuplot                    ; who doesn't like pretty pictures
+       +pomodoro                 ; be fruitful with the tomato technique
+       +present                    ; using org-mode for presentations
+       ;; +pretty
+       +roam2)                     ; wander around notes
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +conda +lsp +pyright +tree-sitter)       ; beautiful is better than ugly
+      ;; (python +conda +lsp +pyright +tree-sitter)       ; beautiful is better than ugly
+      (python +conda +tree-sitter)       ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
-       (racket +lsp +xp)            ; a DSL for DSLs
+      (racket +lsp +xp)            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       (rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+      (rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
-       sh                ; she sells {ba,z,fi}sh shells on the C xor
+      sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       (web +lsp +tree-sitter)               ; the tubes
+      (web +lsp +tree-sitter)               ; the tubes
        ;;yaml              ; JSON, but readable
        ;;zig               ; C, but simpler
 
-       :email
-       (mu4e +org +gmail)
+      :email
+      (mu4e +org +gmail)
        ;;notmuch
        ;;(wanderlust +gmail)
 
-       :app
-       calendar
-       emms
+      :app
+      calendar
+      emms
        ;; mpc
-       everywhere        ; *leave* Emacs!? You must be joking
+      everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
-       rss          ; emacs as an RSS reader
+      rss          ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
 
-       :private
+      :private
        ;; exwmdoom
-       :config
+      :config
        ;;literate
-       (default +bindings +smartparens))
+      (default +bindings +smartparens))
